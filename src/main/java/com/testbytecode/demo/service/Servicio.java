8c5e1e9dc6a685d22aeff78ea0dd09eaffdb3c9a
axl147 <florescapote147@hotmail.com>
2018-11-13 16:22:09 -0400

    service delete listo

--- src/main/java/com/testbytecode/demo/service/Servicio.java
+++ src/main/java/com/testbytecode/demo/service/Servicio.java
@@ -1,5 +1,6 @@
 package com.testbytecode.demo.service;
 
+import org.hibernate.sql.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,31 +9,43 @@ import com.testbytecode.demo.repository.Repositorio;
 
 @Service
 public class Servicio {
-	
+
 	@Autowired
 	Repositorio repo;
-	
-	public boolean crear (Empleado empleado) {
+
+	public boolean crear(Empleado empleado) {
 		try {
 			repo.save(empleado);
 			return true;
-			
+
 		} catch (Exception e) {
 			return false;
-			
+
 		}
-				
-	}
-	public boolean actualizar (Empleado empleado) {
-		try {
-			repo.save(empleado);
-			return true;
-			
-		} catch (Exception e) {
-			return false;
-			
-		}
-				
+
 	}
 
+	public boolean actualizar(Empleado empleado) {
+		try {
+			repo.save(empleado);
+			return true;
+
+		} catch (Exception e) {
+			return false;
+
+		}
+
+	}
+
+	public boolean borrar(String nombre, Long id) {
+		try {
+			repo.delete(repo.findByNombreAndId(nombre, id));
+			return true;
+
+		} catch (Exception e) {
+			return false;
+
+		}
+
+	}
 }
