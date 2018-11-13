33447a76c18f9a98c42fb293b5b1fccf91edb45a
axl147 <florescapote147@hotmail.com>
2018-11-13 15:54:46 -0400

    service crear listo

---
+++ src/main/java/com/testbytecode/demo/service/Servicio.java
@@ -0,0 +1,27 @@
+package com.testbytecode.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.testbytecode.demo.entity.Empleado;
+import com.testbytecode.demo.repository.Repositorio;
+
+@Service
+public class Servicio {
+	
+	@Autowired
+	Repositorio repo;
+	
+	public boolean crear (Empleado empleado) {
+		try {
+			repo.save(empleado);
+			return true;
+			
+		} catch (Exception e) {
+			return false;
+			
+		}
+				
+	}
+
+}
